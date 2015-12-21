38252de1e955b0eba6cb2fea6bd205465632d7cb
Karin Touchie <ktouchie@gmail.com>
2015-12-21 15:16:59 +0100

    Write partition function with printout to test

--- qsort.java
+++ qsort.java
@@ -3,6 +3,28 @@ class qsort {
 
 	public static void main(String[] args) {
         
+        int[] tab = {5,9,4,1,2,7,3,8,6,0};
+        
         
     }
+    
+    public static void partition(int A[], int x, int y) {
+        int p = A[x];
+        int i = x;
+        int k = (A.length-1);
+        int temp;
+        for (i<A.length; i++) {
+            if (A[i]>p) {
+                for (k>0; k--) {
+                    if (A[k]<p) {
+                        temp = A[k];
+                        A[k] = A[i];
+                        A[i] = temp;
+                    }
+                    System.out.println(Arrays.toString(tab))
+                }
+            }
+        }
+    }
+    
 }
